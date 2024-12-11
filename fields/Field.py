904db9386888a8f753c6e8db4837d7253f34e577
Bjorn <128983036+Bjorn4481@users.noreply.github.com>
2024-12-11 20:31:28 +0000

    field update

--- fields/Field.py
+++ fields/Field.py
@@ -4,6 +4,7 @@ class Field:
     def __init__(self):
         self.scoring_locations = []
         self.robots = []
+        self.endgame_duration = 30
         self.match_status = "waiting"
         self.match_time = 150.0 # seconds
         self.env = simpy.Environment()
@@ -21,19 +22,24 @@ class Field:
     def clock(self):
         while True:
             self.set_match_status()
-            yield self.env.timeout(0.1)
+            yield self.env.timeout(1)
 
     def set_match_status(self):
+        self.previous_match_status = self.match_status
         if self.env.now >= self.match_time:
             self.match_status = "finished"
-        elif self.env.now > 15:
+        elif self.env.now >= self.match_time - self.endgame_duration:
+            self.match_status = "endgame"
+        elif self.env.now >= 15:
             self.match_status = "teleop"
-        elif self.env.now > 0:
+        elif self.env.now >= 0:
             self.match_status = "auto"
         else:
             self.match_status = "waiting"
 
-    
+        if self.match_status != self.previous_match_status:
+            print(f"{self.match_status} started at {self.env.now}")
+        
 
     def __repr__(self):
         return self.__class__.__name__
