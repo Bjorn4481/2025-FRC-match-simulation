e981816c18605da0d5b5daef79f9b5362f0944db
TomvdBurgt <148377172+TomvdBurgt@users.noreply.github.com>
2024-12-11 20:30:20 +0000

    started programming gamepieces

--- gamepieces/children/Cargo2022.py
+++ gamepieces/children/Cargo2022.py
@@ -1,6 +1,13 @@
 from ..Gamepiece import Gamepiece
+import time
 
 class Cargo2022(Gamepiece):
     def __init__(self, color):
         super().__init__()
         self.color = color
+
+    def bounce(self):
+        print("bouncing")
+        time.sleep(3)
+
+    
