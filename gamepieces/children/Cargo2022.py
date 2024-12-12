9ca600e87e0e2c8c74d4d71b6b3daec566aec964
TomvdBurgt <148377172+TomvdBurgt@users.noreply.github.com>
2024-12-12 08:30:10 +0000

    added delay in hub (beun)

--- gamepieces/children/Cargo2022.py
+++ gamepieces/children/Cargo2022.py
@@ -1,5 +1,7 @@
 from ..Gamepiece import Gamepiece
 import time
+from random import randint
+from time import sleep
 
 class Cargo2022(Gamepiece):
     def __init__(self, color):
@@ -9,5 +11,10 @@ class Cargo2022(Gamepiece):
     def bounce(self):
         print("bouncing")
         time.sleep(3)
+
+    def HubDelay(self):
+        print("Delayed in hub")
+        sleep(randint(1, 3))
+        print("Scored in hub")
 
     
